3b78253d1a0f4facbf6833467891df24303b50a9
corofides <jamlendrem@gmail.com>
2026-02-21 07:56:58 +0000

    Finish Exercise 04 (14) Outro

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -1,6 +1,29 @@
 // TODO: implement a so-called "Drop bomb": a type that panics when dropped
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
+struct DropBomb {
+    primed: bool,
+}
+
+impl DropBomb {
+    pub fn new() -> Self {
+        Self {
+            primed: true
+        }
+    }
+
+    pub fn defuse(&mut self) {
+        self.primed = false;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if self.primed {
+            panic!("Booom!!!");
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,85 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+use std::ops::Add;
+
+#[derive(PartialEq, Debug, Copy, Clone)]
+pub struct SaturatingU16 {
+    value: u16,
+}
+
+impl Add<SaturatingU16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: SaturatingU16) -> Self::Output {
+        Self {
+            value: self.value.saturating_add(rhs.value)
+        }
+    }
+}
+
+impl Add<&SaturatingU16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: &SaturatingU16) -> Self::Output {
+        Self {
+            value: self.value.saturating_add(rhs.value),
+        }
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        return self.value == *other;
+    }
+}
+
+/*impl Into<u16> for SaturatingU16 {
+    fn into(self) -> u16 {
+        self.value
+    }
+}*/
+
+impl Add<u16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: u16) -> Self::Output {
+        Self {
+            value: self.value.saturating_add(rhs),
+        }
+    }
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        Self {
+            value
+        }
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        let value = value.clone();
+        Self {
+            value: value.into()
+        }
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        Self {
+            value: value.into(),
+        }
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        let value = value.clone();
+        Self {
+            value: value.into(),
+        }
+    }
+}
