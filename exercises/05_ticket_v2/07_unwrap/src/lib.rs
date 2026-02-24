ea9f30b757ed8abe18a0822480d5732c8e8041d1
corofides <jamlendrem@gmail.com>
2026-02-24 06:46:27 +0000

    Finish exercise 05 (08) Error Enums;

--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -2,7 +2,21 @@
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    
+    let ticket = Ticket::new(title.clone(), description.clone(), status.clone());
+
+    match ticket {
+        Ok (ticket) => {
+            ticket
+        },
+        Err(value) => {
+            //println!("{}", value);
+            //
+            let ticket = Ticket::new(title, "Description not provided".to_string(), status).unwrap();
+            ticket
+        }
+    }
+
 }
 
 #[derive(Debug, PartialEq, Clone)]
